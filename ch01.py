17a5c471f92d73988ba19bd9b647f0d749dffd1d
itemhound_shebna <sdfabilloren@up.edu.ph>
2018-10-02 23:30:13 +0800

    items 1-8

--- ch01.py
+++ ch01.py
@@ -1,4 +1,3 @@
-import math
 def gcd(m,n):
 	while m%n != 0:
 		oldm = m
@@ -9,16 +8,21 @@ def gcd(m,n):
 
 		print("n: ",n)
 	return n 
+
 class Fraction:
 	def __str__(self):
 		return str(self.num) + "/" + str(self.den)
+	
 	def __init__(self,top,bottom):
 		self.num = top
 		self.den = bottom
-	
+		if ((type(self.num) != int) or (type(self.den) != int)):
+			raise Exception('This is the exception you expect to handle')
+		if(self.den < 0):
+			self.num = self.num * -1
+			self.den = self.den * -1
 
 
-		# print(gcd(20,10))
 
 	def __add__(self,otherfraction):
 		newnum = self.num*otherfraction.den + self.den*otherfraction.num
@@ -54,10 +58,56 @@ class Fraction:
 		common = gcd(newnum,newden)
 		return Fraction(newnum//common,newden//common)
 
+	def __gt__(self,otherfraction):
+		newnum = self.num * otherfraction.den
+		newden = self.den * otherfraction.num
+
+		return newnum > newden
+
+	def __ge__(self,otherfraction):
+		newnum = self.num * otherfraction.den
+		newden = self.den * otherfraction.num
+
+		return newnum >= newden
+
+	def __lt__(self,otherfraction):
+		newnum = self.num * otherfraction.den
+		newden = self.den * otherfraction.num
+
+		return newnum < newden
+
+	def __le__(self,otherfraction):
+		newnum = self.num * otherfraction.den
+		newden = self.den * otherfraction.num
+
+		return newnum <= newden
+
+	def __ne__(self,otherfraction):
+		newnum = self.num * otherfraction.den
+		newden = self.den * otherfraction.num
+
+		return newnum != newden
+
+	def __radd__(self,otherfraction):
+		return otherfraction.__add__(self)
+
+	def __iadd__(self,otherfraction):
+		print("First ID: ",id(self.num))
+		self.num = self.num*otherfraction.den + self.den*otherfraction.num
+		self.den = self.den * otherfraction.den
+		print("Second ID: ",id(self.num))
+		common = gcd(self.num,self.den)
+		print("newnum: ",self.num)
+		print("newden: ",self.den)
+		return Fraction(self.num//common,self.den//common)
+
+
 
 myf = Fraction(2,3)
 frac1 = Fraction(1,2)
-frac2 = Fraction(3,5)
-total = frac1/frac2
+frac2 = Fraction(2,4)
+# print("frac1: ",frac1)
+# print("frac2: ",frac2)
+
+total = frac1.__iadd__(frac2)
 print(total)
-# print(myf)
